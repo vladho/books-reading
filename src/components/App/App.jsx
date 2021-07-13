f396d464258e33224c563d5786aa0edfe5722eb5
capricornus44 <antonzavalniuk@gmail.com>
2021-07-13 16:55:49 +0200

    added a render of the form depending of the device width and updated styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Container from '../Container/Container';
 import NavigationRoutes from '../Navigation/NavigationRoutes';
+import TrainingPage from '../../pages/TrainingPage';
 
 const App = () => {
     return (
         <Container>
             <NavigationRoutes />
+            <TrainingPage />
         </Container>
     );
 };
