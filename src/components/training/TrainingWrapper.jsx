362dc469dade1b87eb3eecf603f55318e92f4790
capricornus44 <antonzavalniuk@gmail.com>
2021-07-13 08:00:02 +0200

    added Training page template

---
+++ src/components/training/TrainingWrapper.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import styles from './TrainingWrapper.module.scss'
+
+const TrainingWrapper = () => {
+    return (
+        <div>
+            <h1>Training Wrapper</h1> 
+        </div>
+    )
+}
+
+export default TrainingWrapper
