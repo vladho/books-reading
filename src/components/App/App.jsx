01ade0dc802bf5dd6933d01e0b2afc922de34856
capricornus44 <antonzavalniuk@gmail.com>
2021-07-14 09:51:11 +0200

    added device-size mixins instead media queries

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import Container from '../Container/Container';
 import NavigationRoutes from '../Navigation/NavigationRoutes';
 
+import TrainingPage from '../../pages/TrainingPage';
+
 const App = () => {
     return (
         <Container>
             <NavigationRoutes />
+            <TrainingPage />
         </Container>
     );
 };
