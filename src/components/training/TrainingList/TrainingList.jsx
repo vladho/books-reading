3ce232d38f906fa7e548a623cbda5bd59b74bb04
Aleksey Yesin <esinru@gmail.com>
2021-07-14 20:18:04 +0300

    Adds TrainingList placeholder

--- src/components/training/TrainingList/TrainingList.jsx
+++ src/components/training/TrainingList/TrainingList.jsx
@@ -2,19 +2,27 @@ import css from './TrainingList.module.scss';
 import TrainingListItem from '../TrainingListItem';
 import books from '../../../json/trainingListBooks.json';
 
+const placeholder = (
+    <TrainingListItem title="..." author="..." year="..." pages="..." />
+);
+
 export default function TrainingList() {
+    const isArrayNotEmpty = Array.isArray(books) && books.length;
+
     return (
-        <ul>
-            {books.map(({ id, title, author, year, pages }) => (
-                <TrainingListItem
-                    key={id}
-                    title={title}
-                    author={author}
-                    year={year}
-                    pages={pages}
-                    onDelete={() => null}
-                />
-            ))}
+        <ul className={css.list}>
+            {isArrayNotEmpty
+                ? books.map(({ id, title, author, year, pages }) => (
+                      <TrainingListItem
+                          key={id}
+                          title={title}
+                          author={author}
+                          year={year}
+                          pages={pages}
+                          onDelete={() => null}
+                      />
+                  ))
+                : placeholder}
         </ul>
     );
 }
