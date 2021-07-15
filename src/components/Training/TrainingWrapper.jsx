651b16200e1f0246cbe987cebc8c2b7ea736e71a
capricornus44 <antonzavalniuk@gmail.com>
2021-07-15 10:34:48 +0200

    updated training form styles

--- src/components/Training/TrainingWrapper.jsx
+++ src/components/Training/TrainingWrapper.jsx
@@ -2,27 +2,29 @@ import React from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 import TrainingForm from './TrainingForm/TrainingForm';
+import TrainingList from './TrainingList';
 import TrainingModal from './TrainingModal/TrainingModal';
 import styles from './TrainingWrapper.module.scss';
 
 const TrainingWrapper = () => {
-    const isMobile = useMediaQuery({
-        query: '(max-device-width: 767px)',
-    });
+  const isMobile = useMediaQuery({
+    query: '(max-device-width: 767px)',
+  });
 
-    return (
-        <>
-            <div className={styles.wrapper}>
-                {isMobile ? (
-                    <TrainingModal>
-                        <TrainingForm />
-                    </TrainingModal>
-                ) : (
-                    <TrainingForm />
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className={styles.wrapper}>
+        {isMobile ? (
+          <TrainingModal>
+            <TrainingForm />
+          </TrainingModal>
+        ) : (
+          <TrainingForm />
+        )}
+        <TrainingList />
+      </div>
+    </>
+  );
 };
 
 export default TrainingWrapper;
