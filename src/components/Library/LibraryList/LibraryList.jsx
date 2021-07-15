c443440d76967beedd1c1ebf6400f3bbed603f8d
julia_chizhik <for.yu.ch@gmail.com>
2021-07-15 12:20:23 +0300

    summary

--- src/components/Library/LibraryList/LibraryList.jsx
+++ src/components/Library/LibraryList/LibraryList.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from 'react';
+import styles from './LibraryList.module.scss';
 
 function LibraryList({ books }) {
     return (
-        <div>
-            <ul>{books.map(({ id, name, author, year, page }) =>
-                (
-                <li key={id}>
-                    <p>{name}</p>
-                    <p>{author}</p>
-                    <p>{year}</p>
-                    <p>{page}</p>
-                </li>
-            ))}</ul>
+        <div className={styles.container}>
+            <ul className={styles.category}>
+                <p className={styles.categoryTitle}>Маю намір прочитати</p>
+                <ul className={styles.categoryListTitle}>
+                    <li className={styles.categoryListItem}>Назва книги</li>
+                    <li className={styles.categoryListItem}>Автор</li>
+                    <li className={styles.categoryListItem}>Рік</li>
+                    <li className={styles.categoryListItem}>Стор.</li>
+                </ul>
+                <ul className={styles.bookList}>
+                    {books.map(({ id, name, author, year, page }) => (
+                        <li key={id} className={styles.bookListItem}>
+                            <p>{name}</p>
+                            <p>{author}</p>
+                            <p>{year}</p>
+                            <p>{page}</p>
+                        </li>
+                    ))}
+                </ul>
+            </ul>
         </div>
-    )
+    );
 }
 
-export default LibraryList
-
+export default LibraryList;
