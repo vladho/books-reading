69ea52de4b423402ebba4b61f2fd7aaab0ce105c
julia_chizhik <for.yu.ch@gmail.com>
2021-07-16 09:09:38 +0300

    library redux store

--- src/components/Library/LibraryList/LibraryList.jsx
+++ src/components/Library/LibraryList/LibraryList.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import styles from './LibraryList.module.scss';
 
 function LibraryList({ books }) {
-    return (
-        <div className={styles.container}>
-            <ul>
-                <p className={styles.categoryTitle}>Маю намір прочитати</p>
-                <ul className={styles.categoryListTitle}>
-                    <li className={styles.categoryListTitleItemName}>
-                        Назва книги
-                    </li>
-                    <li className={styles.categoryListTitleItemAuthor}>
-                        Автор
-                    </li>
-                    <li className={styles.categoryListTitleItemYear}>Рік</li>
-                    <li className={styles.categoryListTitleItemPage}>Стор.</li>
-                </ul>
-                <ul className={styles.bookList}>
-                    {books.map(({ id, name, author, year, page }) => (
-                        <li key={id} className={styles.bookListItem}>
-                            <p className={styles.bookListItemName}>{name}</p>
-                            <p className={styles.bookListItemAuthor}>
-                                {author}
-                            </p>
-                            <p className={styles.bookListItemYear}>{year}</p>
-                            <p className={styles.bookListItemPage}>{page}</p>
-                        </li>
-                    ))}
-                </ul>
-            </ul>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      {books.length > 0 && (
+        <ul>
+          <p className={styles.categoryTitle}>Маю намір прочитати</p>
+          <ul className={styles.categoryListTitle}>
+            <li className={styles.categoryListTitleItemName}>Назва книги</li>
+            <li className={styles.categoryListTitleItemAuthor}>Автор</li>
+            <li className={styles.categoryListTitleItemYear}>Рік</li>
+            <li className={styles.categoryListTitleItemPage}>Стор.</li>
+          </ul>
+          <ul className={styles.bookList}>
+            {books.map(({ id, name, author, year, page }) => (
+              <li key={id} className={styles.bookListItem}>
+                <p className={styles.bookListItemName}>{name}</p>
+                <p className={styles.bookListItemAuthor}>{author}</p>
+                <p className={styles.bookListItemYear}>{year}</p>
+                <p className={styles.bookListItemPage}>{page}</p>
+              </li>
+            ))}
+          </ul>
+        </ul>
+      )}
+    </div>
+  );
 }
 
-export default LibraryList;
+const mapStateToProps = state => ({
+  books: state.books.items,
+});
+
+export default connect(mapStateToProps)(LibraryList);
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,41 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from './auth/authReducer';
+import booksReducer from './books/booksReducer';
 
 const persistAuthConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-    whitelist: ['token'],
+  key: 'root',
+  version: 1,
+  storage,
+  whitelist: ['token'],
 };
 
 const persistedAuthReducer = persistReducer(persistAuthConfig, authReducer);
 
 export const store = configureStore({
-    reducer: { auth: persistedAuthReducer },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: [
-                    FLUSH,
-                    REHYDRATE,
-                    PAUSE,
-                    PERSIST,
-                    PURGE,
-                    REGISTER,
-                ],
-            },
-        }),
+  reducer: {
+    auth: persistedAuthReducer,
+    books: booksReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
