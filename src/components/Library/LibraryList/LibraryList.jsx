a30e20508d41f5cbd54ad82edfab2cd78d0d0b8a
julia_chizhik <for.yu.ch@gmail.com>
2021-07-16 14:45:11 +0300

    summary

--- src/components/Library/LibraryList/LibraryList.jsx
+++ src/components/Library/LibraryList/LibraryList.jsx
@@ -3,10 +3,58 @@ import { connect } from 'react-redux';
 import styles from './LibraryList.module.scss';
 
 function LibraryList({ books }) {
-  console.log(typeof books.find());
+  console.log(books);
   return (
     <div className={styles.container}>
-      {books.status === 'plan' && (
+      {books.some(book => book.status === 'done') && (
+        <ul>
+          <p className={styles.categoryTitle}>Прочитано</p>
+          <ul className={styles.categoryListTitle}>
+            <li className={styles.categoryListTitleItemName}>Назва книги</li>
+            <li className={styles.categoryListTitleItemAuthor}>Автор</li>
+            <li className={styles.categoryListTitleItemYear}>Рік</li>
+            <li className={styles.categoryListTitleItemPage}>Стор.</li>
+          </ul>
+          <ul className={styles.bookList}>
+            {books.map(
+              ({ _id, title, author, year, totalPages, status }) =>
+                status === 'done' && (
+                  <li key={_id} className={styles.bookListItem}>
+                    <p className={styles.bookListItemName}>{title}</p>
+                    <p className={styles.bookListItemAuthor}>{author}</p>
+                    <p className={styles.bookListItemYear}>{year}</p>
+                    <p className={styles.bookListItemPage}>{totalPages}</p>
+                  </li>
+                ),
+            )}
+          </ul>
+        </ul>
+      )}
+      {books.some(book => book.status === 'read') && (
+        <ul>
+          <p className={styles.categoryTitle}>Читаю</p>
+          <ul className={styles.categoryListTitle}>
+            <li className={styles.categoryListTitleItemName}>Назва книги</li>
+            <li className={styles.categoryListTitleItemAuthor}>Автор</li>
+            <li className={styles.categoryListTitleItemYear}>Рік</li>
+            <li className={styles.categoryListTitleItemPage}>Стор.</li>
+          </ul>
+          <ul className={styles.bookList}>
+            {books.map(
+              ({ _id, title, author, year, totalPages, status }) =>
+                status === 'read' && (
+                  <li key={_id} className={styles.bookListItem}>
+                    <p className={styles.bookListItemName}>{title}</p>
+                    <p className={styles.bookListItemAuthor}>{author}</p>
+                    <p className={styles.bookListItemYear}>{year}</p>
+                    <p className={styles.bookListItemPage}>{totalPages}</p>
+                  </li>
+                ),
+            )}
+          </ul>
+        </ul>
+      )}
+      {books.some(book => book.status === 'plan') && (
         <ul>
           <p className={styles.categoryTitle}>Маю намір прочитати</p>
           <ul className={styles.categoryListTitle}>
@@ -16,14 +64,17 @@ function LibraryList({ books }) {
             <li className={styles.categoryListTitleItemPage}>Стор.</li>
           </ul>
           <ul className={styles.bookList}>
-            {books.map(({ _id, title, author, year, totalPages }) => (
-              <li key={_id} className={styles.bookListItem}>
-                <p className={styles.bookListItemName}>{title}</p>
-                <p className={styles.bookListItemAuthor}>{author}</p>
-                <p className={styles.bookListItemYear}>{year}</p>
-                <p className={styles.bookListItemPage}>{totalPages}</p>
-              </li>
-            ))}
+            {books.map(
+              ({ _id, title, author, year, totalPages, status }) =>
+                status === 'plan' && (
+                  <li key={_id} className={styles.bookListItem}>
+                    <p className={styles.bookListItemName}>{title}</p>
+                    <p className={styles.bookListItemAuthor}>{author}</p>
+                    <p className={styles.bookListItemYear}>{year}</p>
+                    <p className={styles.bookListItemPage}>{totalPages}</p>
+                  </li>
+                ),
+            )}
           </ul>
         </ul>
       )}
