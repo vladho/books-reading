3f69e4aae854afe0386fca2197e82375572287a0
Vlad <vladhopilo@gmail.com>
2021-07-16 19:16:00 +0300

    fix header to Header

--- src/components/header/HeaderWrapper/HeaderWrapper.jsx
+++
@@ -1,9 +0,0 @@
-import css from './HeaderWrapper.module.scss';
-
-export default function HeaderWrapper({ children }) {
-    return (
-        <div className={css.wrapper}>
-            <div className={css.wrapperFixed}>{children}</div>
-        </div>
-    );
-}
--- src/components/header/index.js
+++
@@ -1,4 +0,0 @@
-import Header from './Header';
-import AppBar from './AppBar/AppBar';
-import HeaderWrapper from './HeaderWrapper/HeaderWrapper';
-export { Header, AppBar, HeaderWrapper };
