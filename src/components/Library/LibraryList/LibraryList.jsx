2c741752e342a93accc5a6097877dcebf5713c88
julia_chizhik <for.yu.ch@gmail.com>
2021-07-17 10:51:47 +0300

    summary

--- src/components/Library/LibraryList/LibraryList.jsx
+++ src/components/Library/LibraryList/LibraryList.jsx
@@ -1,29 +1,41 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { ReactSVG } from 'react-svg';
 import styles from './LibraryList.module.scss';
+import bookUnRead from '../../../assets/icons/bookUnRead.svg';
+import bookRead from '../../../assets/icons/bookRead.svg';
+import bookReadDone from '../../../assets/icons/bookReadDone.svg';
 
 function LibraryList({ books }) {
-  console.log(books);
   return (
     <div className={styles.container}>
       {books.some(book => book.status === 'done') && (
-        <ul>
+        <ul className={styles.category}>
           <p className={styles.categoryTitle}>Прочитано</p>
           <ul className={styles.categoryListTitle}>
-            <li className={styles.categoryListTitleItemName}>Назва книги</li>
-            <li className={styles.categoryListTitleItemAuthor}>Автор</li>
-            <li className={styles.categoryListTitleItemYear}>Рік</li>
-            <li className={styles.categoryListTitleItemPage}>Стор.</li>
+            <li className={styles.categoryListTitleItemNameDone}>
+              Назва книги
+            </li>
+            <li className={styles.categoryListTitleItemAuthorDone}>Автор</li>
+            <li className={styles.categoryListTitleItemYearDone}>Рік</li>
+            <li className={styles.categoryListTitleItemPageDone}>Стор.</li>
+            <li className={styles.categoryListTitleItemRate}>Рейтинг книги</li>
           </ul>
           <ul className={styles.bookList}>
             {books.map(
               ({ _id, title, author, year, totalPages, status }) =>
                 status === 'done' && (
                   <li key={_id} className={styles.bookListItem}>
-                    <p className={styles.bookListItemName}>{title}</p>
-                    <p className={styles.bookListItemAuthor}>{author}</p>
-                    <p className={styles.bookListItemYear}>{year}</p>
-                    <p className={styles.bookListItemPage}>{totalPages}</p>
+                    <ReactSVG src={bookReadDone} className={styles.icons} />
+                    <p className={styles.bookListItemNameDone}>{title}</p>
+                    <p className={styles.bookListItemAuthorDone}>{author}</p>
+                    <p className={styles.bookListItemYearDone}>{year}</p>
+                    <p className={styles.bookListItemPageDone}>{totalPages}</p>
+                    <div className={styles.stars}></div>
+                    <button type="button" className={styles.buttonRezume}>
+                      Резюме
+                    </button>
                   </li>
                 ),
             )}
@@ -31,7 +43,7 @@ function LibraryList({ books }) {
         </ul>
       )}
       {books.some(book => book.status === 'read') && (
-        <ul>
+        <ul className={styles.category}>
           <p className={styles.categoryTitle}>Читаю</p>
           <ul className={styles.categoryListTitle}>
             <li className={styles.categoryListTitleItemName}>Назва книги</li>
@@ -44,6 +56,7 @@ function LibraryList({ books }) {
               ({ _id, title, author, year, totalPages, status }) =>
                 status === 'read' && (
                   <li key={_id} className={styles.bookListItem}>
+                    <ReactSVG src={bookRead} className={styles.icons} />
                     <p className={styles.bookListItemName}>{title}</p>
                     <p className={styles.bookListItemAuthor}>{author}</p>
                     <p className={styles.bookListItemYear}>{year}</p>
@@ -55,7 +68,7 @@ function LibraryList({ books }) {
         </ul>
       )}
       {books.some(book => book.status === 'plan') && (
-        <ul>
+        <ul className={styles.category}>
           <p className={styles.categoryTitle}>Маю намір прочитати</p>
           <ul className={styles.categoryListTitle}>
             <li className={styles.categoryListTitleItemName}>Назва книги</li>
@@ -68,6 +81,7 @@ function LibraryList({ books }) {
               ({ _id, title, author, year, totalPages, status }) =>
                 status === 'plan' && (
                   <li key={_id} className={styles.bookListItem}>
+                    <ReactSVG src={bookUnRead} className={styles.icons} />
                     <p className={styles.bookListItemName}>{title}</p>
                     <p className={styles.bookListItemAuthor}>{author}</p>
                     <p className={styles.bookListItemYear}>{year}</p>
@@ -76,6 +90,11 @@ function LibraryList({ books }) {
                 ),
             )}
           </ul>
+          <NavLink to="/training" className={styles.link}>
+            <button type="button" className={styles.button}>
+              Далі
+            </button>
+          </NavLink>
         </ul>
       )}
     </div>
