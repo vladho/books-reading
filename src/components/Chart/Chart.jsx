f4a0186c52bb5cfc57dbe695d65754a91ed8113d
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-07-21 17:52:09 +0300

    chart connected to backend, username in header

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
-// import { useSelector } from 'react-redux';
-// import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import moment from 'moment';
 
 import { Line } from 'react-chartjs-2';
 
-// import { tasksSls } from '../../../redux/tasks';
-// import { sprintsSls } from '../../../redux/sprints';
+import { trainingSelectors } from '../../redux/training';
+import { booksSelectors } from '../../redux/books';
 
 import css from './Chart.module.scss';
 
@@ -21,24 +22,103 @@ const Chart = () => {
   const planedPages = plan.reduce((acc, day) => (acc += day), 0);
   const avaregePgesPerDay = planedPages / plan.length;
 
+  const getSelectBooks = useSelector(trainingSelectors.getSelectBooks);
+  // const getResults = useSelector(trainingSelectors.getResults); // пока есть тестовый массив
+  const planedBooks = useSelector(booksSelectors.getPlanBooks);
+  const startDate = useSelector(trainingSelectors.selectStartDate);
+  const endDate = useSelector(trainingSelectors.selectEndDate);
+  const { selectedIds } = useParams();
+
+  // ==== общее кол-во страниц
+  const planedPagesArray = () => {
+    return getSelectBooks.map(trainingBook => {
+      return trainingBook.totalPages;
+    });
+  };
+
+  const totalPages = planedPagesArray().reduce((acc, planedPage) => {
+    return acc + planedPage;
+  }, 0);
+  // console.log(totalPages);
+
+  // ===== кол-во дней
+  const getResults = [
+    {
+      date: '2021-07-21',
+      pages: '25',
+    },
+    {
+      date: '2021-07-21',
+      pages: '50',
+    },
+    {
+      date: '2021-07-22',
+      pages: '38',
+    },
+    {
+      date: '2021-07-22',
+      pages: '42',
+    },
+    {
+      date: '2021-07-23',
+      pages: '70',
+    },
+  ];
+  const dateNowStr = moment().format('YYYY-MM-DD');
+
+  // сравнение текущей даты с датой старта
+  // const actualStartDate = (dateNowStr, startDate) => {
+  //   return dateNowStr > startDate ? dateNowStr === startDate : startDate;
+  // };
+  // console.log('actualStartDate :>> ', actualStartDate());
+
+  const getResultForDay = resultsDate =>
+    getResults
+      .filter(({ date }) => date === resultsDate)
+      .reduce((acc, { pages }) => (acc += Number(pages)), 0);
+
+  const getReadedStats = ({ totalPages, startDate, endDate }) => {
+    const endDateUpdate = endDate.split('-').join('');
+
+    const daysToEnd =
+      +moment(endDateUpdate, 'YYYYMMDD').fromNow().split(' ')[1] + 1;
+    // console.log('daysToEnd :>> ', daysToEnd);
+
+    const avaregePgesPerDay = totalPages / daysToEnd;
+    // console.log('avaregePgesPerDay :>> ', avaregePgesPerDay);
+    const factPages = getResultForDay(dateNowStr);
+    return {
+      avaregePgesPerDay,
+      factPages,
+    };
+  };
+
+  // const { avaregePgesPerDay, factPages } = getReadedStats;
+  // console.log(getReadedStats);
+  const stats = getReadedStats({ totalPages, endDate });
+  // console.log('stats :>> ', stats);
+  // ==== Массивы для построения графика====
+  const getPlanedLine = avaregePgesPerDay => {
+    let arr = [];
+    arr.push(avaregePgesPerDay);
+    return arr;
+  };
+  // console.log('getPlanedLine :>> ', getPlanedLine);
+
+  // const getActLine = factPages => {
+  //   let arr = [];
+  //   arr.push(factPages);
+  //   return arr;
+  // };
+  // console.log('actLine :>> ', actLine);
+
   const chartLine = () => {
     setChartData({
+      // labels: ['Start', ...getDatesArray()[0]],
       labels: ['So', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Su'],
       datasets: [
         {
           label: 'Plan',
-          fill: false,
-          lineTension: 0.3,
-          borderColor: '#ff6b08',
-          pointBackgroundColor: '#ff6b08',
-          pointHoverRadius: 10,
-          pointRadius: 8,
-          pointHitRadius: 10,
-          //   data: [planedHours, ...getWastedLine()],
-          data: plan,
-        },
-        {
-          label: 'Act',
           fill: false,
           lineTension: 0.3,
           borderColor: '#091e3f',
@@ -46,7 +126,20 @@ const Chart = () => {
           pointHoverRadius: 10,
           pointRadius: 8,
           pointHitRadius: 10,
-          //   data: [...getStreightLine()],
+          // data: [...getPlanedLine()],
+          data: plan,
+        },
+        {
+          label: 'Act',
+          fill: false,
+          lineTension: 0.3,
+          borderColor: '#ff6b08',
+          pointBackgroundColor: '#ff6b08',
+
+          pointHoverRadius: 10,
+          pointRadius: 8,
+          pointHitRadius: 10,
+          //   data: [...getActLine()],
           data: act,
         },
       ],
@@ -72,7 +165,9 @@ const Chart = () => {
       <div className={css.chartBox}>
         <p className={css.title}>
           Amount of pages / Day{' '}
-          <span className={css.avaregePgesPerDay}>{avaregePgesPerDay}</span>
+          <span className={css.avaregePgesPerDay}>
+            {stats.avaregePgesPerDay}
+          </span>
         </p>
         <div className={css.lineBox}>
           <ul className={css.lineList}>
