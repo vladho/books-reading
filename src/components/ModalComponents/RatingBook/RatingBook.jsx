7ad8d1fd695f7e9612aa11146be0bf258d0b973f
capricornus44 <antonzavalniuk@gmail.com>
2021-07-25 00:09:08 +0200

    added ukrainian translation for the resume book modal

--- src/components/ModalComponents/RatingBook/RatingBook.jsx
+++ src/components/ModalComponents/RatingBook/RatingBook.jsx
@@ -1,19 +1,29 @@
+import React, { useContext } from 'react';
 import CancelButton from '../../common/ModalButton/CancelButton/CancelButton';
 import DoneButton from '../../common/ModalButton/DoneButton/DoneButton';
 import ChooseRating from './ChooseRating/ChooseRating';
 import styles from './RatingBook.module.scss';
+import { LangContext } from '../../App/App';
 
 const RatingBook = ({ toogleModal }) => {
+  const { language } = useContext(LangContext);
+
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Choose rating of the book</h2>
+      <h2 className={styles.title}>
+        {language.libraryPage.resumeModal.rating}
+      </h2>
       <ChooseRating />
-      <h2 className={styles.title}>Resume</h2>
+      <h2 className={styles.title}>
+        {language.libraryPage.resumeModal.textFieldTitle}
+      </h2>
       <textarea placeholder="..." className={styles.textarea}></textarea>
       <CancelButton styleBtn={styles.canselBtn} onCbClick={toogleModal}>
-        Back
+        {language.libraryPage.resumeModal.backBtn}
       </CancelButton>
-      <DoneButton styleBtn={styles.doneBtn}>Save</DoneButton>
+      <DoneButton styleBtn={styles.doneBtn}>
+        {language.libraryPage.resumeModal.saveBtn}
+      </DoneButton>
     </div>
   );
 };
