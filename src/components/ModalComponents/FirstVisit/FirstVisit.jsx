8ba56c95dd804d914d1df3d52ac25a45407b744b
capricornus44 <antonzavalniuk@gmail.com>
2021-07-25 00:00:11 +0200

    added ukrainian translation for the first vist modal

--- src/components/ModalComponents/FirstVisit/FirstVisit.jsx
+++ src/components/ModalComponents/FirstVisit/FirstVisit.jsx
@@ -1,38 +1,46 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from './FirstVisit.module.scss';
 import { ReactComponent as BookIcon } from '../../../assets/icons/bookUnRead.svg';
 import { ReactComponent as Flag } from '../../../assets/icons/Flag.svg';
 import { ReactComponent as ArrowRight } from '../../../assets/icons/ArrowRight.svg';
 import {} from '../../../';
 import DoneButton from '../../common/ModalButton/DoneButton/DoneButton';
+import { LangContext } from '../../App/App';
 
 const FirstVisit = () => {
+  const { language } = useContext(LangContext);
+
   return (
     <div className={styles.container}>
       <ul className={styles.list}>
         <li>
-          <h2 className={styles.step}>Step 1.</h2>
+          <h2 className={styles.step}>{language.libraryPage.greet.step1}</h2>
           <div className={styles.titleGoup}>
             <BookIcon className={styles.bookIcon} />
-            <h3 className={styles.title}>Create your own library</h3>
+            <h3 className={styles.title}>
+              {language.libraryPage.greet.step1_title}
+            </h3>
           </div>
           <div className={styles.textGoup}>
             <ArrowRight />
             <p className={styles.text}>
-              Add there books which you are going to read.
+              {language.libraryPage.greet.step1_text}
             </p>
           </div>
         </li>
         <li>
-          <h2 className={styles.step}>Step 2.</h2>
+          <h2 className={styles.step}>{language.libraryPage.greet.step2}</h2>
           <div className={styles.titleGoup}>
             <Flag className={styles.bookIcon} />
-            <h3 className={styles.title}>Create your first training</h3>
+            <h3 className={styles.title}>
+              {' '}
+              {language.libraryPage.greet.step2_title}
+            </h3>
           </div>
           <div className={styles.textGoup}>
             <ArrowRight />
             <p className={styles.text}>
-              Set a goal, choose period, start training.
+              {language.libraryPage.greet.step2_text}
             </p>
           </div>
         </li>
