01657ca60bbe85e8ffda9e150386932ff28d3554
capricornus44 <antonzavalniuk@gmail.com>
2021-07-25 00:04:21 +0200

    added ukrainian translation for the modal during logout

--- src/components/ModalComponents/LeaveApp/LeaveApp.jsx
+++ src/components/ModalComponents/LeaveApp/LeaveApp.jsx
@@ -1,19 +1,19 @@
+import React, { useContext } from 'react';
 import CancelButton from '../../common/ModalButton/CancelButton/CancelButton';
 import DoneButton from '../../common/ModalButton/DoneButton/DoneButton';
 import styles from './LeaveApp.module.scss';
+import { LangContext } from '../../App/App';
 
 const LeaveApp = ({ toogleModal }) => {
+  const { language } = useContext(LangContext);
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>
-        The changes you made will be lost if you navigate away from this
-        application
-      </h2>
+      <h2 className={styles.title}>{language.trainingPage.logoutModal.desc}</h2>
       <div className={styles.btnGroup}>
         <CancelButton styleBtn={styles.btn} onCbClick={toogleModal}>
-          Сancel
+          {language.trainingPage.logoutModal.cancelBtn}
         </CancelButton>
-        <DoneButton>Leave this app</DoneButton>
+        <DoneButton>{language.trainingPage.logoutModal.exitBtn}</DoneButton>
       </div>
     </div>
   );
