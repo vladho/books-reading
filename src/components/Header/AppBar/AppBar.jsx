b42a53b6218eaac8fcd8965d952e8d9f0cd04766
Oleksandr Myrvoda <mirvoda2@gmail.com>
2021-07-26 16:48:51 +0300

    isStarted fixes

--- src/components/Header/AppBar/AppBar.jsx
+++ src/components/Header/AppBar/AppBar.jsx
@@ -1,19 +1,16 @@
 import React, { useState, useContext } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { GoHome, GoBook } from 'react-icons/go';
-import { authSls, authOps } from '../../../redux/auth';
+import { authSls } from '../../../redux/auth';
 
 import css from './AppBar.module.scss';
 import { NavLink } from 'react-router-dom';
 import { LangContext } from '../../App/App';
 
-import NestingModal from '../../ModalHoc/NestingModal/NestingModal';
 import LeaveApp from '../../ModalComponents/LeaveApp/LeaveApp';
 
 export default function AppBar() {
   const { language } = useContext(LangContext);
-  // const dispatch = useDispatch();
-  // const onLogout = () => dispatch(authOps.logOut());
   const [showModal, setShowModal] = useState(false);
 
   const isShowModal = () => {
